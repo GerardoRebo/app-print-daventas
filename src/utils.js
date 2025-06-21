f9ce918f3fa88023bca66da019b7f38426fdfe8b
GerardoRebo <81606616+GerardoRebo@users.noreply.github.com>
2025-06-21 10:10:10 -0600

    remove tabindex attributes from Vuetify data table elements and add utility function

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 export const removeVuetifyTableTabIndexes = () => {
   console.log("aewf");
-  
+
   document.querySelectorAll('.v-data-table td[tabindex="0"]').forEach(el => {
     console.log("fv1r");
-    
+
     el.removeAttribute('tabindex');
   });
   document.querySelectorAll('.v-data-table th[tabindex="0"]').forEach(el => {
